e7140fcc9dde8f42f8bb13210860bd18779e6e99
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2016-03-02 16:50:07 +0100

    partial add

--- des/desclient.py
+++ des/desclient.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging
+import logging, datetime
 from resync.client import Client
 
 class DesClient(Client):
@@ -11,15 +11,15 @@ class DesClient(Client):
         self.logger = logging.getLogger(__name__)
         self.sync_status = []
 
-    def log_status(self, in_sync=True, incremental=False, audit=False,
-                   same=None, created=0, updated=0, deleted=0, to_delete=0):
+    def log_status(self, in_sync=None, incremental=False, audit=False,
+                   same=None, created=0, updated=0, deleted=0, to_delete=0, exception=None):
         self.sync_status.append(SourceStatus(self.mapper.default_src_uri(), in_sync, incremental, audit, same,
-                                             created, updated, deleted, to_delete))
+                                             created, updated, deleted, to_delete, exception))
         super().log_status(in_sync, incremental, audit, same, created, updated, deleted, to_delete)
 
     def sync_status_to_file(self, filename):
         with open(filename, 'w') as file:
-            file.write("%s\n" % "uri,in_sync,incremental,audit,same,created,updated,deleted,to_delete")
+            file.write("%s\n" % "date,uri,in_sync,incremental,audit,same,created,updated,deleted,to_delete,exception")
             for item in self.sync_status:
                 file.write("%s\n" % item)
             file.close()
@@ -28,7 +28,8 @@ class DesClient(Client):
 
 class SourceStatus(object):
 
-    def __init__(self, uri, in_sync, incremental, audit, same, created, updated, deleted, to_delete):
+    def __init__(self, uri, in_sync, incremental, audit, same, created, updated, deleted, to_delete, exception):
+        self.datetime = datetime.datetime.now()
         self.uri = uri
         self.in_sync = in_sync
         self.incremental = incremental
@@ -38,9 +39,12 @@ class SourceStatus(object):
         self.updated = updated
         self.deleted = deleted
         self.to_delete = to_delete
+        self.exception = exception
 
     def __str__(self):
         s = "\""
+        s += str(self.datetime)
+        s += "\",\""
         s += self.uri
         s += "\",\""
         s += str(self.in_sync)
@@ -58,5 +62,7 @@ class SourceStatus(object):
         s += str(self.deleted)
         s += "\",\""
         s += str(self.to_delete)
+        s += "\",\""
+        s += str(self.exception)
         s += "\""
         return s
---
+++ des/processor.py
@@ -0,0 +1,192 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+import requests, urllib.parse, xml, abc
+import xml.etree.ElementTree as ET
+import resync
+from enum import Enum
+from resync.sitemap import Sitemap
+
+WELLKNOWN_RESOURCE = ".well-known/resourcesync"
+CAPA_DESCRIPTION = "description"
+CAPA_CAPABILITYLIST = "capabilitylist"
+CAPA_RESOURCELIST = "resourcelist"
+CAPA_RESOURCEDUMP = "resourcedump"
+CAPA_CHANGELIST = "changelist"
+CAPA_CHANGEDUMP = "changedump"
+
+
+class Status(Enum):
+    init = 1
+    read_error = 2
+    document = 3
+    processed_with_exceptions = 4
+    processed = 5
+
+class Processor(object):
+    """
+    Reads a sitemap from a uri and turns it into a resync.resource_container.ResourceContainer
+    """
+
+    def __init__(self, source_uri, expected_capability):
+        """
+
+        :param source_uri: the uri of the sitemap that must be read
+        :param expected_capability: the expected capability of the sitemap
+        :return: None
+        """
+        self.logger = logging.getLogger(__name__)
+        self.status = Status.init
+        self.source_uri = source_uri
+        self.capability = expected_capability
+
+        self.source_status = None
+        self.exceptions = []
+        self.source_document = None
+        self.describedby_url = None
+        self.up_url = None
+
+    def read_source(self):
+        """
+
+        :return:
+        """
+        session = requests.Session()
+        try:
+            response = session.get(self.source_uri)
+            self.source_status = response.status_code
+            self.logger.debug("Read %s, status %s" % (self.source_uri, str(self.source_status)))
+            assert self.source_status == 200, "Invalid response status: %d" % self.source_status
+
+            text = response.text
+            root = ET.fromstring(text)
+            sitemap = Sitemap()
+            self.source_document = sitemap.parse_xml(etree=ET.ElementTree(root))
+            # the source_document is a resync.resource_container.ResourceContainer
+            capability = self.source_document.capability
+            assert capability == self.capability, \
+                "Capability is not %s but %s" % (self.capability, capability)
+            self.describedby_url = self.source_document.describedby
+            self.up_url = self.source_document.up
+            self.status = Status.document
+
+        except requests.exceptions.ConnectionError as err:
+            self.logger.debug("%s No connection: %s" % (self.source_uri, str(err)))
+            self.status = Status.read_error
+            self.exceptions.append(err)
+
+        except xml.etree.ElementTree.ParseError as err:
+            self.logger.debug("%s ParseError: %s" % (self.source_uri, str(err)))
+            self.status = Status.read_error
+            self.exceptions.append(err)
+
+        except resync.sitemap.SitemapParseError as err:
+            self.logger.debug("%s Unreadable source: %s" % (self.source_uri, str(err)))
+            self.status = Status.read_error
+            self.exceptions.append(err)
+
+        except AssertionError as err:
+            self.logger.debug("%s Error: %s" % (self.source_uri, str(err)))
+            self.status = Status.read_error
+            self.exceptions.append(err)
+
+        finally:
+            session.close()
+
+        return self.status == Status.document
+
+    def has_exceptions(self):
+        return len(self.exceptions) != 0
+
+    @abc.abstractmethod
+    def process_source(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+    def __assert_document__(self):
+        """
+        Make sure the source document is read and correct.
+        :return: True if source document is read and correct, False otherwise
+        """
+        self.logger.debug("Start %s on %s" % (self.__class__.__name__, self.source_uri))
+        if not self.status is Status.document:
+            self.read_source()
+        if not self.status is Status.document:
+            self.logger.debug("Not processing %s because of previous errors" % self.source_uri)
+        return self.status == Status.document
+
+
+class Discoverer(Processor):
+
+    def __init__(self, base_uri):
+        self.logger = logging.getLogger(__name__)
+        # urllib.parse.urljoin leaves out the last part of a path if it doesn't end with '/'
+        if base_uri.endswith("/"):
+            self.base_uri = base_uri
+        else:
+            self.base_uri = base_uri + "/"
+        wellknown = urllib.parse.urljoin(self.base_uri, WELLKNOWN_RESOURCE)
+        super(Discoverer, self).__init__(wellknown, CAPA_DESCRIPTION)
+
+    def process_source(self):
+        if not self.__assert_document__():
+            return
+
+        # the source document is a source description
+        for resource in self.source_document.resources:
+            # it contains links to capabilitylists
+            capaproc = Capaproc(resource.uri)
+            capaproc.process_source()
+            self.exceptions.extend(capaproc.exceptions)
+
+        self.status = Status.processed_with_exceptions if self.has_exceptions() else Status.processed
+
+
+class Capaproc(Processor):
+
+    def __init__(self, uri):
+        self.logger = logging.getLogger(__name__)
+        super(Capaproc, self).__init__(uri, CAPA_CAPABILITYLIST)
+
+    def process_source(self):
+        if not self.__assert_document__():
+            return
+
+        # the source document is a capability list
+        for resource in self.source_document.resources:
+            capability = resource.capability
+            if capability == CAPA_RESOURCELIST:
+                reliproc = Reliproc(resource.uri)
+                reliproc.process_source()
+                self.exceptions.extend(reliproc.exceptions)
+            elif capability == CAPA_RESOURCEDUMP:
+                pass
+            elif capability == CAPA_CHANGELIST:
+                pass
+            elif capability == CAPA_CHANGEDUMP:
+                pass
+            else:
+                self.logger.debug("Unknown capability %s in %s" % (capability, self.source_uri))
+                self.exceptions.append("Unknown capability %s in %s" % (capability, self.source_uri))
+
+        self.status = Status.processed_with_exceptions if self.has_exceptions() else Status.processed
+
+
+class Reliproc(Processor):
+
+    def __init__(self, uri):
+        self.logger = logging.getLogger(__name__)
+        super(Reliproc, self).__init__(uri, CAPA_RESOURCELIST)
+
+    def process_source(self):
+        if not self.__assert_document__():
+            return
+
+        # The source document is a resourcelist
+        for resource in self.source_document.resources:
+            print(resource.hash)
+
